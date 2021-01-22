620989c0897b522113efa9bc840bc8750f48a396
Trent Conley <trentconley@gmail.com>
2021-01-21 19:39:24 -0600

    working thougg eclipse on M1

---
+++ Entropy Capstone/src/simulation/SimulationLauncher.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package simulation;
+
+/**
+ * @author trentconley
+ *
+ */
+public class SimulationLauncher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
