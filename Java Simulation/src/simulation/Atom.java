6d0e75a4253d231f0814f12dd16d4a8c041b0bf6
Trent Conley <trentconley@gmail.com>
2021-02-06 18:59:37 -0600

    working

--- Java Simulation/src/simulation/Atom.java
+++ Java Simulation/src/simulation/Atom.java
@@ -24,95 +24,12 @@ public abstract class Atom
 		v = copyV;
 	}
 
-
-//	This method will return the collision time of the atom to a Vector
-
-//	the Vector will be in the form y = m * x + b
-
-//	I am just calculating the intersection point of two Vectors.
-
-
-/*
-	public double collisionVector(double m1, double b1)
-	{
-//		2 at the end of the letter refers to the atom, 1 refers to the boundary Vector
-//		this function will be useful when computing the collisions between spheres I think.
-		double m2 = magnitude * Math.tan(theta); 
-		double b2 = m2 * (-c.getX()) + c.getY();
-		double a = 0;
-		double xcolpos = (b2-b1)/(m1-m2);
-		double ycolpos = m1 * xcolpos + b1;
-		return 0;
-*		y = m * x + b
-*		y  = m (x - x1)+ y1
-*		y = m2 (-x2) + y2 because the x value is 0 on the y intercept and this is equal to b2
-*		and if we have the mx+b form for the other, we can set them equal to each other
-* 
-	}
-*/
-
-
-//	for vertical or horizontal Vectors
-	
-	/*
-	public double collisionVector(boolean vert_horiz, double x,  double y)
-	{
-		if (vert_horiz)
-		{
-//			This will assume that the user inputs the double x as the 
-//			slope and the double y as the y intercept
-			return collisionVector(x, y);
-		}
-		if (x == -1)
-		{
-			if (Math.cos(theta) < 0)
-			{
-				return -1;
-	//			the negative 1 will mean that it will never happen, and such will be interpreted in
-	//			the program. a cosine equaling 0 will result in an undefined function. 
-			}
-//			 we are returning the time time until collision, also all the particles will be 
-//			inside the box
-			return (x-c.getX()) / (v.getMagnitude() * Math.cos(v.getTheta()));
-		}
-		return (y - c.getY())/ (v.getMagnitude() * Math.sin(theta)); // numerator is distance to travel,
-		//Denominator is the rate at which it is changing, thereby giving the seconds until
-		//collision
-	}
-	*/
-
-	/*
-	 * returns the x velocity component of the atom
-	 */
-	
-	/*
-	public double getTimeCollision(Atom a)
-	{
-		
-//		using two clone because will hold address that point to the heap so I can 
-//		modify both atoms in another function and have it return something while not 
-//		messing with the originals. 
-		Atom clone_movingAtom = new Atom(xpos, ypos, magnitude, theta, radius);
-		Atom clone_stationaryAtom = new Atom(a.getCoordinates(),  a.get_magnitude(), 
-				a.get_theta(), a.get_radius());
-		double centerx = a.get_xpos();
-		return 0;
-	}
-	*/
 	
 	public void make_relative(Atom mov, Atom stat)
 	{
 //		going to make one atom stationary relative to the other. 
 		return;
 	}
-/*
- * For the checking (if they will collide) part, I think we can do it by simply 
- * calculating the shortest distance between the first circle's velocity Vector, 
- * and the second circle's center, and checking if the distance is greater than R1+R2, 
- * so the only thing remains is to find the collision point.
- */
-	
-
 	
 
 //	These four methods will allow the me to get all of the class scope variables.
--- Java Simulation/src/simulation/SimulationLauncher.java
+++ Java Simulation/src/simulation/SimulationLauncher.java
@@ -11,24 +11,27 @@ import java.util.*;
  */
 // I will be using argon in the simulation because it is the cheapest noble gas, and I want
 // to actually build this simulation
-// we can make the simulation a cubic millimeter. I think that is a good idea. 
+// we can make the simulation a cubic centimeter. I think that is a good idea. 
 public class SimulationLauncher 
 	{
 //	the number of atoms in the system
-	public static final int NUM_ATOMS = 10;
+	public static final int NUM_ATOMS = 100;
 	
 //	all atoms will be assigned a single temperature, but as the simulation continues they will 
 //	naturally fall into a Maxwell-Boltzmann distribution. 
 	public static final BigDecimal STARTING_TEMPERATURE_SYSTEM = new BigDecimal ("273.0");
 	
 //	This should equal 1.38064852 * 10^-23, boltzmann constant
-	public static final BigDecimal k = new BigDecimal(".000000000000000000000138064852");
+	public static final BigDecimal k = new BigDecimal("1.38064852e-23");
 	
-	public static final double SIZE_X = 0;
+//	the size of the simulation will be in micro meters. 
+	public static final double SIZE_X = 10000;
 	
-	public static final double SIZE_Y = 0;
+	public static final double SIZE_Y = 10000;
 	
-
+//	1 amu  = 1.6605e-27 kg
+	public static final BigDecimal atomicMassUnitsToKilograms = new BigDecimal("1.6605e-27");
+	
 	/**
 	 * @param args
 	 * next step: collision mechanism. 
@@ -58,14 +61,13 @@ public class SimulationLauncher
 //		(3kT/m)^(1/2) where k is the 
 //		k = 1.38064852 × 10^-23 m^2 kg s^-2 K-1
 		BigDecimal three = new BigDecimal("3.0"); 
-		BigDecimal mass = new BigDecimal (String.valueOf(Argon.MASS));
-		print(mass);
+		BigDecimal massAtomicUnits = new BigDecimal (String.valueOf((Argon.MASS)));
+		BigDecimal massKilograms = massAtomicUnits.multiply(atomicMassUnitsToKilograms);
 		BigDecimal ktemp = k.multiply(STARTING_TEMPERATURE_SYSTEM);
 		BigDecimal ktempthree = ktemp.multiply(three);
 		MathContext mc = new MathContext(2, RoundingMode.HALF_UP);
-		BigDecimal ktempthreemass = ktempthree.divide(mass, mc);
+		BigDecimal ktempthreemass = ktempthree.divide(massKilograms, mc);
 		BigDecimal velocityBigDecimal = ktempthreemass.sqrt(mc);
-		print(velocityBigDecimal);
 		double velocity = velocityBigDecimal.doubleValue();
 		
 //		I created the lines above to be redundant in the calculation of the velocity. 
@@ -88,13 +90,14 @@ public class SimulationLauncher
 	{
 		for (int i = 0; i < a.length; i ++)
 		{
+//			lol
 			print(a[i]);
 		}
 	}
 	
 	public static void print(Atom a)
 	{
-//		I honestly this its dumb how java makes you type out a bunch of stuff for printing
+//		I honestly think its dumb how java makes you type out a bunch of stuff for printing
 		System.out.println(a);
 	}
 	
