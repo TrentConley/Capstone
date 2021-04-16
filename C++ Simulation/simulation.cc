c033c7f9b2b76e22392492ab3700440e47d30e7a
Trent Conley <trentconley@gmail.com>
2021-04-16 11:35:24 -0500

    added JBox2D sim

--- C++ Simulation/simulation.cc
+++ C++ Simulation/simulation.cc
@@ -7,7 +7,9 @@ using namespace std;
 
 int main (int argc, char** argv)
 {
-    cout << "hello world" << endl;
+    b2Vec2 gravity(0.0f, -10.0f);
+    b2World world(gravity);
+    cout << "hello world" << endl; 
     return 0; 
 }
 
---
+++ JBox2D Simulation/src/simulation/SimulationLauncher.java
@@ -0,0 +1,31 @@
+package simulation;
+
+
+public class SimulationLauncher 
+{
+
+	public static void main(String[] args) 
+	{
+		double a = 0.11;
+		print(a);
+		// TODO Auto-generated method stub
+
+	}
+	public static void print (double[] arr)
+	{
+		
+	}
+	public static void print(double d)
+	{
+		System.out.println(d);
+	}
+	public static void print (String s)
+	{
+		System.out.println(s);
+	}
+	public static void print (int i)
+	{
+		System.out.println(i);
+	}
+
+}
