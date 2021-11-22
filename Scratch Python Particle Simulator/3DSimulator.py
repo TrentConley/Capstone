5aa46f418bc50d6b5dad2b11f2eb932e0b79e556
Trent Conley <trentconley@gmail.com>
2021-11-22 11:54:00 -0600

    fixing grid error

--- Scratch Python Particle Simulator/3DSimulator.py
+++ Scratch Python Particle Simulator/3DSimulator.py
@@ -4,7 +4,7 @@
 
 
 
-# 3D simulation of bouncing balls
+# 3D simulation of bouncing particles
 #
 # author: Konstantin Lübeck (Embedded Systems, Universtiy of Tuebingen)
 # adapted from: Stephan Schirrecker (schirrecker) 
@@ -29,13 +29,13 @@ cor = 1.0
 # 1 millisecond delta t
 delta_t = 0.001
 
-# number of balls to simulate
-num_balls = 100
+# number of particles to simulate
+num_particles = 10
 
 # size of the x, y, and z axis
 size = 10
 
-# colors of the balls
+# colors of the particles
 colors = ['b', 'g', 'r', 'c', 'm', 'y']
 
 # inital view angles
@@ -58,9 +58,9 @@ ax.set_zlim(zlim)
 
 ax.view_init(elev, azim)
 
-balls = []
-
-class Ball:
+particles = [] # need to to set up as global
+grid = [] # need to set up as global 
+class particle:
     def __init__(self, xyz, v, fmt, radius = 0.1):
         self.xyz = np.array(xyz)
         self.v = np.array(v)
@@ -79,27 +79,27 @@ class Ball:
         return []
 
     def update(self, grid):
-        # ball hits lower x wall 
+        # particle hits lower x wall 
         if self.xyz[0] <= xlim[0]:
             self.v[0] = cor * np.abs(self.v[0])
 
-        # ball hits upper x wall
+        # particle hits upper x wall
         elif self.xyz[0] >= xlim[1]:
             self.v[0] = - cor * np.abs(self.v[0])
 
-        # ball hits lower y wall
+        # particle hits lower y wall
         if self.xyz[1] <= ylim[0]:
             self.v[1] = cor * np.abs(self.v[1])
 
-        # ball hits upper y wall
+        # particle hits upper y wall
         elif self.xyz[1] >= ylim[1]:
             self.v[1] = - cor * np.abs(self.v[1])
 
-        # ball hits lower z wall
+        # particle hits lower z wall
         if self.xyz[2] <= zlim[0]:
             self.v[2] = cor * np.abs(self.v[2])
 
-        # ball hits upper z wall
+        # particle hits upper z wall
         elif self.xyz[2] >= zlim[1]:
             self.v[2] = - cor * np.abs(self.v[2])
 
@@ -109,46 +109,47 @@ class Ball:
         collision_info = self.does_elastic_collision_occur()
         if (collision_info[0]):
             self.deal_with_elastic_collision(collision_info[1])
-        self.xyz += self.v
-
-        # make sure the balls stay inside of the canvas
+        update_position(in_particle = self, g = grid)
+        print("we are here")
+        quit()
+        # make sure the particles stay inside of the canvas
         self.xyz[0] = np.clip(self.xyz[0], xlim[0], xlim[1])
         self.xyz[1] = np.clip(self.xyz[1], ylim[0], ylim[1])
         self.xyz[2] = np.clip(self.xyz[2], zlim[0], zlim[1])
 
-        # draw ball
+        # draw particle
         self.scatter.set_xdata(self.xyz[0])
         self.scatter.set_ydata(self.xyz[1])
         self.scatter.set_3d_properties(self.xyz[2])
-    
+
+def update_position(in_particle = None, g = None):
+    # update grid here
+    x = in_particle.xyz[0]
+    y = in_particle.xyz[1]
+    print(g[math.floor(x)][math.floor(y)])
+    print(in_particle)
+
+    g[math.floor(x)][math.floor(y)].remove(in_particle)
+    new_x, new_y = x + in_particle.v[0]*TIME_STEP, y + in_particle.v[1]*TIME_STEP
+    # try:
+    g[math.floor(new_x)][math.floor(new_y)].append(in_particle)
+    print('updated')
+    quit()
         
-def main():
+    # except IndexError:
+    #     print("x: " + str(math.floor(new_x)))
+    #     print("y: " + str(math.floor(new_y)))
+    #     print(g[new_x][new_y])
+    in_particle.xyz[0] = new_x
+    in_particle[1] = new_y
 
-
-    # generate balls with random position, velocity, and color
-
-
-    for i in np.arange(0,num_balls):
-        xyz = np.random.rand(1,3)[0]*size
-        xyz[1] = 5 # for 2 dimensional dynamics
-        v = np.random.rand(1,3)[0]*0.1
-        v[1] = 0 # for 2 dimensional dynamics
-        fmt = str(colors[np.random.randint(0,len(colors))] + 'o')
-        balls.append(Ball(xyz, v, fmt))
-
-    grid = [[list() for y in range (ylim[0], ylim[1])] for x in range (xlim[0], xlim[1])] # for efficient computation
-    fill_grid(g = grid, ep = balls)
-
-    ani = FuncAnimation(fig, update, frames=np.arange(0,0.5,delta_t), init_func=init, interval=10, blit=True, repeat=True)
-    #ani.save('animation.gif', writer='imagemagick', fps=30) # will be huge for ORNL
-    plt.show()
-
-
+    pass  
+        
 def fill_grid(g = None, ep = None):
-    # for p in ep:
-    #     x = math.floor(p.pos[0])
-    #     y = math.floor(p.pos[1])
-    #     g[x][y].append(p)
+    for p in ep:
+        x = math.floor(p.xyz[0])
+        y = math.floor(p.xyz[1])
+        g[x][y].append(p)
     pass
 
 def init():
@@ -158,10 +159,10 @@ def update(t):
 
     global elev, azim
 
-    for ball in balls:
-        ball.update(grid)
+    for particle in particles:
+        particle.update(grid)
 
-    artists = [ball.scatter for ball in balls]
+    artists = [particle.scatter for particle in particles]
   
     # rotate view
     azim = azim + t/2
@@ -169,7 +170,26 @@ def update(t):
     
     artists.append(ax)
 
-    return artists 
+    return artists
 
-main()
+    # generate particles with random position, velocity, and color
+
+for i in np.arange(0,num_particles):
+    xyz = np.random.rand(1,3)[0]*size
+    xyz[1] = 5 # for 2 dimensional dynamics
+    v = np.random.rand(1,3)[0]*0.1
+    v[1] = 0 # for 2 dimensional dynamics
+    fmt = str(colors[np.random.randint(0,len(colors))] + 'o')
+    particles.append(particle(xyz, v, fmt))
+
+grid = [[list() for y in range (ylim[0], ylim[1])] for x in range (xlim[0], xlim[1])] # for efficient computation
+fill_grid(g = grid, ep = particles)
+print(grid)
+ani = FuncAnimation(fig, update, frames=np.arange(0,0.5,delta_t), init_func=init, interval=10, blit=True, repeat=True)
+#ani.save('animation.gif', writer='imagemagick', fps=30) # will be huge for ORNL
+plt.show()
+
+
+
+
 
